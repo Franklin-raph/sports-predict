9f7634d021af77ab91e75219e9f789233940d014
franklin-raph <igboekwulusifranklin@gmail.com>
2023-12-05 19:33:07 +0100

    bug fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
+import Logo from "../../assets/logo png (1).png"
 
 const Navbar = ({setShowSignIn, setShowSignUp, baseUrl}) => {
 
@@ -42,7 +43,7 @@ const Navbar = ({setShowSignIn, setShowSignUp, baseUrl}) => {
 
   return (
       <nav className='flex items-center justify-between w-full pb-1 relative flex-col' style={{ borderBottom:"1px solid #4F3D3D" }}>
-        <img src="/public/logo png (1).png" alt="" width="30%" className='mb-2'/>
+        <img src={Logo} alt="" width="30%" className='mb-2'/>
         {!user && 
           <div className='bg-[#4f3d3d] text-white w-full text-center rounded-[10px] mb-5 px-3'>
             <h4 className='mb-[1rem] mt-[3rem] font-bold text-xl text-[#847777]'>You do not have an account logged in</h4>
